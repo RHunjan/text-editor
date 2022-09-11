6d634bd33f0bad830a5d24ff8da7d4c87373223a
ReenaH <Reena_Hunjan@yahoo.com>
2022-09-11 19:00:33 -0400

    manifest json and plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack. DONE
 
 module.exports = () => {
   return {
@@ -17,11 +15,44 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
+      plugins: [
+      // TODO: Add and configure workbox plugins for a service worker and manifest file.
+       new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Text Editor'
+       }),
+       new InjectManifest({
+      swSrc: './src/sw.js',
+      swDest: 'service-worker.js',
+          }),
+      new WebpackPwaManifest({
+     name: 'Text Editor',
+     short_name: 'JATE',
+     description: 'Keep track of contacts!',
+     background_color: '#272822',
+     theme_color: '#272822',
+    start_url: './',
+    publicPath: './',
+    icons: [
+    {
+      src: path.resolve('src/images/logo.png'),
+      sizes: [96, 128, 192, 256, 384, 512],
+      destination: path.join('assets', 'icons'),
+    },
+    {
+      src: path.resolve('src/images/icon-manifest.png'),
+      size: '1024x1024',
+      destination: path.join('assets', 'icons'),
+      purpose: 'maskable'
+    }
+  ],
+})
+
       
     ],
 
     module: {
+      // TODO: Add CSS loaders and babel to webpack. DONE
       rules: [
         {
          test: /\.css$/i,
